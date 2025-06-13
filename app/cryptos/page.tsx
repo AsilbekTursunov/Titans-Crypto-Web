ced2fb96f32a1d032eb6dcbc22dbc09835cf66d2
asilbek_tursun <148640148+AsilbekTursunov@users.noreply.github.com>
2025-06-13 16:18:50 +0500

    update error

--- app/cryptos/page.tsx
+++ app/cryptos/page.tsx
@@ -1,13 +1,17 @@
 import CoinsList from '@/components/CoinsList'
+import Loader from '@/components/Loader'
 import { getCoins } from '@/server'
 import React, { use } from 'react'
 
 const Cryptos = () => {
 	const coins = use(getCoins(100))
 
-  return <div className='container mx-auto mt-10'>
-    <CoinsList coins={coins.coins} />
-  </div>
+	if (!coins) return <Loader />
+	return (
+		<div className='container mx-auto mt-10'>
+			<CoinsList coins={coins?.coins} />
+		</div>
+	)
 }
 
 export default Cryptos
--- app/exchanges/page.tsx
+++ app/exchanges/page.tsx
@@ -1,16 +1,23 @@
-import ExchangeCard from '@/components/cards/Exchange';
-import Exchange from '@/components/cards/Exchange';
+import ExchangeCard from '@/components/cards/Exchange'
+import Exchange from '@/components/cards/Exchange'
+import Loader from '@/components/Loader'
 import { getExchanges } from '@/server'
 import React, { use } from 'react'
 
 const Exchanges = () => {
-  const exchanges:IExchange[] = use(getExchanges(30)) 
-  
-  return <div className='container mx-auto mt-10 mb-10'> 
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-      {exchanges.map(item => <ExchangeCard key={item.id} {...item} />)}
-    </div>
-  </div>
+	const exchanges: IExchange[] = use(getExchanges(30))
+
+	if (!exchanges) return <Loader />
+
+	return (
+		<div className='container mx-auto mt-10 mb-10'>
+			<div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'>
+				{exchanges.map(item => (
+					<ExchangeCard key={item.id} {...item} />
+				))}
+			</div>
+		</div>
+	)
 }
 
 export default Exchanges
