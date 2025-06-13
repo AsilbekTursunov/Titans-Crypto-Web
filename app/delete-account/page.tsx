dd59c2e15210aabf206690bcce2e18c6342b76e9
asilbek_tursun <148640148+AsilbekTursunov@users.noreply.github.com>
2025-06-13 16:11:36 +0500

    delete account pag

---
+++ app/delete-account/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const DeleteAccountPage = () => {
+  return (
+    <div>
+      Hi Every one If you enter during 90 days Your informations are delete automaticly
+    </div>
+  )
+}
+
+export default DeleteAccountPage
